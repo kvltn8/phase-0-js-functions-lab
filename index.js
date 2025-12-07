fa4c62599cb7644f3fa962e693071785213b79f0
Kaltun <kaltunabdullahi070@gmail.com>
2025-12-07 14:59:21 +0300

    completed lab on js functions

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
+function calculateTax(amount){
+  return amount * 0.10
+}
 
+function convertToUpperCase(text){
+  return text.toUpperCase();
+}
+
+function findMaximum(num1, num2){
+  if(num1 > num2){
+    return num1;
+  } else{
+    return num2;
+  }
+}
+
+function isPalindrome(word){
+  const letters = word.split("");
+  const reversedLetters = letters.reverse();
+  const reversedWord = reversedLetters.join("");
+
+  if (word === reversedWord){
+    return true;
+  } else{
+    return false;
+  }
+}
+
+function calculateDiscountedPrice(originalPrice , discountPercentage){
+  const discountAmount = (discountPercentage/100) * originalPrice;
+  const finalPrice =originalPrice - discountAmount;
+  return finalPrice;
+}
 
 
 
